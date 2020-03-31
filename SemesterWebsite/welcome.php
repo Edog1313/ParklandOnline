0e76739c96552db683e5cdf8eabb6a549d79f6cb
Eli Remington -Aug-17-2018 <eremington2@bcst01.parkland.edu>
2020-03-30 20:41:24 -0500

    TEST COMMIT

--- SemesterWebsite/welcome.php
+++ SemesterWebsite/welcome.php
@@ -13,5 +13,76 @@ checkAccount("none");
 ?>
 </head>
 <body>
-This is the welcome page for our project TYEst
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<html>
+<head>
+<title>Welcome at CSC155 001DR Email Website</title>
+
+<style>
+.center {
+  margin: auto;
+  width: 90%;
+  font-size: 8px;
+  color: grey;
+  padding: 10px;
+  text-align: center;
+}
+
+.bar{
+  width: 80%;
+  height: 4px;
+  background-color: #9B9B9B;
+  margin: 0 auto;
+}
+
+</style>
+
+
+<body>
+
+<div class="center">
+
+<h1>Welcome to Eli's Semester Website</h1>
+</div>
+
+<div class="bar"></div></head>
+<h1>I'm glad you could make it.</h1>
+
 </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
